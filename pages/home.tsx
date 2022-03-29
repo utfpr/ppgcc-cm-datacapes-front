4f0f43710eb884c9365695e8df1e4df5e05a29de
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-03-28 23:58:18 -0300

    feat: 🎸 Listing component w/ pagination
    ✅ Closes: #6

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,14 +1,18 @@
 import { useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+import { Listing } from "../components/Listing";
+import { Pagination } from "../components/Pagination";
 import { XMLUploadButton } from "../components/XMLUploadButton";
 import { XMLUploadModal } from "../components/XMLUploadModal";
 
 export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
+  const [page, setPage] = useState(1);
   return (
     <div>
       <XMLUploadButton />
       <XMLUploadModal isOpen={isOpen} onClose={onClose} />
+      <Listing />
     </div>
   );
 }
