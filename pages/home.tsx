1e3032892ecc26bab67c1bc0a127eeb65088ba2d
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-03-30 17:18:44 -0300

    refactor: 💡 Data listing map

--- pages/home.tsx
+++ pages/home.tsx
@@ -5,6 +5,27 @@ import { Pagination } from "../components/Pagination";
 import { XMLUploadButton } from "../components/XMLUploadButton";
 import { XMLUploadModal } from "../components/XMLUploadModal";
 
+const dataTest = [
+  {
+    id: "aasd34-32-df-sdf3",
+    title: "Teste a b c sjdbsakjd dgfs asd asd ",
+    author: "joaozinho",
+    email: "joaozinho@gmail.com",
+  },
+  {
+    id: "aasd34-32-sddsf-sdf3",
+    title: "hhhhhhhhhhhhhh a b c sjdbsakjd dgfs asd asd ",
+    author: "dssssdghh",
+    email: "aaaafffgfg@gmail.com",
+  },
+  {
+    id: "aasd34-32-asasf-sdf3",
+    title: "fdsdsfdfds a b c sjdbsakjd dgfs asd asd ",
+    author: "sadasdsaddsa",
+    email: "gfgffggfhr@gmail.com",
+  },
+];
+
 export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [page, setPage] = useState(1);
@@ -12,7 +33,7 @@ export default function Home() {
     <div>
       <XMLUploadButton />
       <XMLUploadModal isOpen={isOpen} onClose={onClose} />
-      <Listing />
+      <Listing data={dataTest} />
     </div>
   );
 }
