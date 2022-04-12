3a585631e9800ae0fbbc9246efcad9568ca82684
JuanFSR <juanfrangel15@gmail.com>
2022-04-11 21:49:22 -0300

    feat: 🎸 Adding Yup to help with the form validation

--- components/LoginForm/index.tsx
+++ components/LoginForm/index.tsx
@@ -2,21 +2,38 @@ import {
   FormControl,
   FormLabel,
   Input,
-  FormErrorMessage,
-  FormHelperText,
   VStack,
   Button,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginForm() {
   // function errorMessageExample() {
   // function errorMessageExample() {
-  const [input, setInput] = useState("");
+  // const [input, setInput] = useState("");
 
-  const handleInputChange = (e) => setInput(e.target.value);
+  // const handleInputChange = (e) => setInput(e.target.value);
 
-  const isError = input === "";
+  const formik = useFormik({
+    initialValues: {
+      nickname: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      nickname: Yup.string()
+        .required("É necessário inserir um apelido")
+        .min(6, "Nickname é muito curto"),
+
+      password: Yup.string().required("É necessário inserir uma senha"),
+    }),
+    onSubmit: (values, actions) => {
+      alert(JSON.stringify(values, null, 2));
+      actions.resetForm();
+    },
+  });
+
+  // const isError = input === "";
 
   return (
     <VStack
@@ -25,33 +42,27 @@ export default function LoginForm() {
       w={{ base: "90%", md: 500 }}
       h="100vh"
       justifyContent="center"
+      onSubmit={formik.handleSubmit}
     >
-      <FormControl isInvalid={isError}>
-        <FormLabel htmlFor="email">Email</FormLabel>
+      <FormControl>
+        <FormLabel>Apelido</FormLabel>
         <Input
-          id="email"
-          type="email"
-          value={input}
-          onChange={handleInputChange}
+          id="nickname"
+          type="text"
+          value={formik.values.nickname}
+          onChange={formik.handleChange}
         />
-        {!isError ? (
-          <FormHelperText>
-            Entre utilizando o email da universidade tecnológica federal do
-            paraná.
-          </FormHelperText>
-        ) : (
-          <FormErrorMessage>É necessário um email.</FormErrorMessage>
-        )}
-
+      </FormControl>
+      <FormControl>
         <FormLabel htmlFor="password">Senha</FormLabel>
         <Input
           id="password"
           type="password"
-          value={input}
-          onChange={handleInputChange}
+          value={formik.values.password}
+          onChange={formik.handleChange}
         ></Input>
       </FormControl>
-      <Button colorScheme="yellow" variant="solid">
+      <Button colorScheme="yellow" variant="solid" type="submit">
         Login
       </Button>
     </VStack>
