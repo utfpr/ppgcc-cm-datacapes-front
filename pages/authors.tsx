9ac58e198e52021e5fcf0fe49b7e1314c8c67e4b
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-04-25 21:59:20 -0300

    feat: 🎸 Listing card author w/ pagination & search

--- pages/authors.tsx
+++ pages/authors.tsx
@@ -1,9 +1,28 @@
+import { Box, Flex, theme, Wrap, WrapItem } from "@chakra-ui/react";
 import { CardAuthor } from "../components/CardAuthor";
+import { Pagination } from "../components/Pagination";
+import { SearchBox } from "../components/SearchBox";
 
 export default function Authors() {
   return (
-    <div>
-      <CardAuthor />
-    </div>
+    <Box m="8">
+      <Flex justify={"center"} mb="10">
+        <SearchBox />
+      </Flex>
+      <Wrap spacing={"3"} justify="center">
+        {[...new Array(8)].map((_, index) => (
+          <WrapItem key={index}>
+            <CardAuthor />
+          </WrapItem>
+        ))}
+      </Wrap>
+      <Pagination
+        onPageChange={() => {}}
+        totalCountOfRegisters={100}
+        currentPage={1}
+        registersPerPage={8}
+        colorList={"dark.black"}
+      />
+    </Box>
   );
 }
