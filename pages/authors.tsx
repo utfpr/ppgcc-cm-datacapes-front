d2efc8ee0ac1a2ce739af34cb80011475f488e6f
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-05-09 21:43:08 -0300

    feat: 🎸 Listing test authors

--- pages/authors.tsx
+++ pages/authors.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
+import { useAuthors } from "../services/hooks/useAuthors";
+
 export default function Authors() {
+  const [page, setPage] = useState(1);
+  const { data, error, isLoading, isFetching } = useAuthors(
+    page /*, {
+        initialData: users,
+    }*/
+  );
+
+  useEffect(() => {
+    console.log(data);
+  }, [page]);
+
   return (
     <div>
-      <h1>Hello Next.js</h1>
+      <button onClick={() => setPage(page + 1)}>Next</button>
     </div>
   );
 }
