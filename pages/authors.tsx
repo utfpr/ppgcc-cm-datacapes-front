3ea852a0a097138a0b858838900cb0675d319379
Caio Theodoro <caio-eduardotheodoro@hotmail.com>
2022-05-16 21:37:25 -0300

    pr adjust

--- pages/authors.tsx
+++ pages/authors.tsx
@@ -3,10 +3,8 @@ import { useAuthors } from "../services/hooks/useAuthors";
 
 export default function Authors() {
   const [page, setPage] = useState(1);
-  const { data, error, isLoading, isFetching } = useAuthors(
-    page /*, {
-        initialData: users,
-    }*/
+  const { data} = useAuthors(
+    page
   );
 
   useEffect(() => {
